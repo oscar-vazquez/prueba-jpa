c7d72a3e40da27d0c1196bd5fec95902d71843c0
Oscar Vazquez <oscar.a.vazquez@enel.com>
2018-03-02 16:48:23 -0300

    Initial commit

---
+++ src/main/java/pruebas/Cliente.java
@@ -0,0 +1,36 @@
+package pruebas;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@ApiModel(value = "Cliente")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Cliente {
+    @ApiModelProperty
+    private int numero;
+
+    @ApiModelProperty
+    private String nombre;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
